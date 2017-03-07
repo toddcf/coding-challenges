0a78f7bfe090394edb7f6298c51fd5b1c227d545
Todd Croak-Falen <toddcf@gmail.com>
2017-03-06 23:39:08 -0800

    Initial setup.

---
+++ interview-cake/stock-price.js
@@ -0,0 +1,24 @@
+// https://www.interviewcake.com/question/javascript/stock-price
+
+var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+
+var getMaxProfit = function( arr ) {
+	var min = arr[0];
+	var max = arr[0];
+	for ( var i = 0; i < arr.length; i++) {
+		if ( arr[i] < min ) {
+			min = arr[i];
+		}
+	};
+	for ( var i = 0; i < arr.length; i++) {
+		if ( arr[i] > max ) {
+			max = arr[i];
+		}
+	};
+	var profit = max - min;
+	return profit;
+	console.log(profit);
+};
+
+getMaxProfit(stockPricesYesterday);
+// returns 6 (buying for $5 and selling for $11)
