67d1af8d236afa399857a66588fea8ec709f600a
Todd Croak-Falen <toddcf@gmail.com>
2017-03-07 15:58:45 -0800

    Added second array with more variations to test tougher scenarios.

--- interview-cake/stock-price.js
+++ interview-cake/stock-price.js
@@ -1,6 +1,7 @@
 // https://www.interviewcake.com/question/javascript/stock-price
 
 var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+var upAndDown = [12, 24, 30, 20, 10, 20, 21];
 
 var getMaxProfit = function( arr ) {
 	var min = arr[0];
@@ -21,4 +22,5 @@ var getMaxProfit = function( arr ) {
 };
 
 getMaxProfit(stockPricesYesterday);
-// returns 6 (buying for $5 and selling for $11)
+// returns $6 (buying for $5 and selling for $11)
+getMaxProfit(upAndDown);
