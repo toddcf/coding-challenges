664f6f86701a814c71e70bbd69bf031ef5dfd8a4
Todd Croak-Falen <toddcf@gmail.com>
2017-03-08 01:42:52 -0800

    Began pseudo coding the structure.

--- interview-cake/product-of-other-numbers/product-of-other-numbers.js
+++ interview-cake/product-of-other-numbers/product-of-other-numbers.js
@@ -1,1 +1,17 @@
 // alert("product.js file is linked!");
+
+var numbers = [1, 7, 3, 4];
+
+// Write a function that returns the product of all the other numbers in the array -- but NOT the number at the index you're on.
+
+var products = function( arr ) {
+	// For loop
+	for ( i = 0; i < numbers.length; i++ ) {
+		// In the first iteration, multiply the numbers EXCEPT FOR 1:
+		console.log();
+		return ;
+}
+}
+
+// Call function:
+products( numbers );
