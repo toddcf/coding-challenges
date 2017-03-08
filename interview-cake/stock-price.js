7c7c92057ee45bdd9918fef35146c84f3277a2f9
Todd Croak-Falen <toddcf@gmail.com>
2017-03-07 16:10:30 -0800

    Created empty array to store any profits calculated in the for loop in. Used only one for loop this time.

--- interview-cake/stock-price.js
+++ interview-cake/stock-price.js
@@ -2,25 +2,33 @@
 
 var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
 var upAndDown = [12, 24, 30, 20, 10, 20, 21];
+// Empty array. Any profits will be pushed here during the for loop:
+var profit = [];
 
 var getMaxProfit = function( arr ) {
+	// Set the first number in the array to the min just to get started:
 	var min = arr[0];
-	var max = arr[0];
+	// Declare profit variable in this scope:
+	var profit;
 	for ( var i = 0; i < arr.length; i++) {
+		// If current i is less than the min, make it the new min:
 		if ( arr[i] < min ) {
 			min = arr[i];
 		}
-	};
-	for ( var i = 0; i < arr.length; i++) {
-		if ( arr[i] > max ) {
-			max = arr[i];
+		// If next number is higher than the current min, subtract the current min from that number:
+		else {
+			profit = i - min;
+			// Push profit to a the profit array:
+
 		}
 	};
-	var profit = max - min;
-	return profit;
-	console.log(profit);
+	// Use Math.max to locate the max profit from the profit array:
+	var maxProfit = Math.max(profit);
+	return maxProfit;
+	console.log("Max Profit: $" + maxProfit);
 };
 
 getMaxProfit(stockPricesYesterday);
-// returns $6 (buying for $5 and selling for $11)
+// returns $6 (buying for $5 and selling for $11).
 getMaxProfit(upAndDown);
+// Should return $18 (buying for $12 and selling for $30).
